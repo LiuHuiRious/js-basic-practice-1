b5a61b28a59e807b05858acd7885d04caef6ab7a
LiuHui <1132787527@qq.com>
2017-05-29 19:51:23 +0800

    完成练习

--- main/main.js
+++ main/main.js
@@ -1,9 +1,25 @@
 // Write your cade below:
-function caclRemaindar() {};
+function caclRemaindar(a,b) {
+    return a%b;
+};
 
-function caclSum() {};
+function caclSum(arr) {
+    var sum=0;
+    for(var i=0;i<arr.length;i++) {
+        sum=sum+arr[i]
+        ;    }
+    return sum;
+};
 
-function caclSumInConditon() {};
+function caclSumInConditon(arr,num) {
+    var sum=0;
+    for(var i=0;i<arr.length;i++) {
+        if(arr[i]<num){
+            sum=sum+arr[i];
+        }
+    }
+    return sum;
+};
 
 module.exports = {
     caclRemaindar,
